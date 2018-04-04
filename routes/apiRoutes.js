2e3633f6a0b4574a8601752f790e63b5b68a6ac0
Mia <miacrandell@gmail.com>
2018-04-04 12:28:11 -0600

    set up nyt specific api

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,13 @@
 const axios = require("axios");
 const router = require("express").Router();
 
+const authKey = "b9f91d369ff59547cd47b931d8cbc56b:0:74623931";
+
 router.get("/articles", (req, res) => {
   axios
-    .get("http://www.newyorktimes.com/api/", { params: req.query })
+    .get("https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" +
+    authKey + "&q="
+    , { params: req.query })
     .then(({ data: { results } }) => res.json(results))
     .catch(err => res.status(422).json(err));
     console.log(results);
