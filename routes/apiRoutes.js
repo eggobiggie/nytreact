cfc89c3e7ad1bf1c360d949e9a4611269b700f3a
Mia <miacrandell@gmail.com>
2018-04-07 12:17:54 -0600

    refresh collection and save per search

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,17 +4,12 @@ const router = require("express").Router();
 const db = require('../models');
 
 router.get("/search/:topic/:startYear/:endYear", (req, res) => {
+
+  //Drop collection so it can be refreshed per search
+  mongoose.connection.db.dropCollection("articles");
   
   const apiKey = "d4adf4a8f6034776a1f3fc735080ffc6";
     
-    /*
-     * The format of the URL is
-     * https://api.nytimes.com/svc/search/v2/articlesearch.json
-     * ?api-key=d4adf4a8f6034776a1f3fc735080ffc6
-     * &q=%22steam-clams%22
-     * &begin_date=19910101
-     * &end_date=20181231
-     */ 
     let searchUrl = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=' +
     apiKey + "&q=" + req.params.topic + "&begin_date=" + req.params.startYear + 
     "0101&end_date=" + req.params.endYear + "1231";
@@ -39,13 +34,13 @@ router.get("/search/:topic/:startYear/:endYear", (req, res) => {
       console.log(articleArray);
       
       //Save information to the database
-      db.Article.create(response.data).then(function (dbArticle) {
+      db.Article.create(articleArray).then(function (dbArticle) {
         console.log(dbArticle);
       }).catch(function (err) {
         console.log("THE ERROR IS: " + err);
         return res.json(err);
       });
-      res.json(response.data);
+      res.json(articleArray);
     })
 
     .catch(err => res.status(422).json(err));
@@ -53,9 +48,7 @@ router.get("/search/:topic/:startYear/:endYear", (req, res) => {
     
 });
 
-
 router.get("/fetchArticles", (req, res) => {
-  // res.send("Test");
   db.Article.find({}).then(function (dbArticle) {
     res.json(dbArticle);
   }).catch(function (err) {
@@ -65,50 +58,3 @@ router.get("/fetchArticles", (req, res) => {
 
 module.exports = router;
 
-
-
-// router.get("/articles", (req, res) => {
-// module.exports = (req, res) => {
-//   const authKey = "b9f91d369ff59547cd47b931d8cbc56b:0:74623931";
-
-//   axios
-//     .get("https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" +
-//       authKey + "&q=", { params: req.query })
-//     .then(({ data: { results } }) => res.json(results))
-//     .catch(err => res.status(422).json(err));
-
-  // let results = {
-  //   title: "",
-  //   pub_date: "",
-  //   url: ""
-  // };
-
-  // mongoose.connection.db.dropCollection("articles");
-
-  // const authKey = "b9f91d369ff59547cd47b931d8cbc56b:0:74623931";
-
-  // axios.get("https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" +
-  //   authKey + "&q=",
-  //   { params: req.query })
-  //   .then(({ data: { results } }) => 
-  //   {
-  //     for (let i = 0; i < data.length; i++) {
-  //       results = {
-  //         title: data.results[i].headline.main,
-  //         pub_date: data.results[i].pub_date,
-  //         url: data.results[i].web_url
-  //       }
-  //     }
-  //     res.json(results)
-  //   })
-  //   .catch(err => res.status(422).json(err));
-  // console.log(res);
-
-  // db.Article.create(results).then(function (dbArticle) {
-  //   console.log(dbArticle);
-  // }).catch(function (err) {
-  //   console.log("THE ERROR IS: " + err);
-  //   return res.json(err);
-  // });
-// });
-// }
